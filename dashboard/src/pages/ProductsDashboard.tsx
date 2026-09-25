8469429d4fd325fc6d893308fd461450e2cabf55
MohHamdallah1 <moh.lo.zma1@gmail.com>
2026-09-25 08:20:31 +0300

    Remove verified-stale import download parser import

--- dashboard/src/pages/ProductsDashboard.tsx
+++ dashboard/src/pages/ProductsDashboard.tsx
@@ -45,7 +45,6 @@ import {
 } from "@/lib/exactMoney";
 import {
   parsePackageUoms,
-  parseProductImportErrorPage,
   parseProductTrackingDefaults,
   parseProductTrackingMutation,
   parseSimpleProductPriceMutationResponse,
