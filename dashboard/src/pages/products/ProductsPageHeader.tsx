5feedc81f8777ea913a6728b4c317879d08b3847
MohHamdallah1 <moh.lo.zma1@gmail.com>
2026-09-26 11:32:16 +0300

    Use one split Product add action

--- dashboard/src/pages/products/ProductsPageHeader.tsx
+++ dashboard/src/pages/products/ProductsPageHeader.tsx
@@ -7,7 +7,6 @@ import {
 
 import { WorkspaceTopBar } from "@/components/dashboard/WorkspaceTopBar";
 import { ProductsAddAction } from "@/pages/products/header/ProductsAddAction";
-import { ProductsAddMenu } from "@/pages/products/header/ProductsAddMenu";
 import { ProductsCatalogToolsMenu } from "@/pages/products/header/ProductsCatalogToolsMenu";
 
 type Props = {
@@ -106,8 +105,14 @@ export function ProductsPageHeader({
             canCreateSimpleProduct={
               canCreateSimpleProduct
             }
+            canImportProducts={
+              canImportProducts
+            }
             onOpenCreateProduct={
               onOpenCreateProduct
+            }
+            onOpenImport={
+              onOpenImport
             }
           />
         </div>
